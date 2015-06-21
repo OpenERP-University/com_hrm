8acc0ddb35508fca69b2af1a0a9659647a08df8d
nhannd92 <nhannd92@gmail.com>
2015-06-22 00:06:44 +0700

    Update hrm.php

--- administrator/hrm.php
+++ administrator/hrm.php
@@ -1,11 +1,34 @@
 <?php
 /**
- * @version     1.0.0
- * @package     com_hrm
- * @copyright   Bản quyền (C) 2015. Các quyền đều được bảo vệ.
- * @license     bản quyền mã nguồn mở GNU phiên bản 2
- * @author      Tran Xuan Duc <ductranxuan.29710@gmail.com> - http://facebook.com/ducsatthuttd
- */
+* Open ERP University - HUMG
+*
+* Copyright (c) 2015 Open ERP University <https://github.com/OpenERP-University> - Hanoi University of Mining and Geology (HUMG)- http://humg.edu.vn 
+*
+* This component is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This component is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this component; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*
+* 
+* @version 1.0.0
+* @package com_hrm
+* @copyright Copyright (c) 2015 Open ERP University - Hanoi University of Mining and Geology (HUMG)- http://humg.edu.vn 
+* @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+* @group OpenERP University - Chuyen Trung Tran <chuyentt@gmail.com> 
+* @author Leader: Tran Xuan Duc <ductranxuan.29710@gmail.com> 
+* @author Dinh Trong Nghia <dinhtrongnghia92@gmail.com> 
+* @author Nguyen Dau Hoang <hoangdau17592@gmail.com> 
+* @author Nguyen Duc Nhan <nhannd92@gmail.com> 
+*/
 
 
 // no direct access
